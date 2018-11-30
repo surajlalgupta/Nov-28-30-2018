f998d940d4a89b4601c3bda72382e2fc02330225
prabhu-durasoft <iamprabhu@yahoo.com>
2018-11-30 11:34:33 +0530

    Day03

--- Day03/day03_apollo_react_client/src/BookList.js
+++ Day03/day03_apollo_react_client/src/BookList.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
 
 
 class BookList extends Component {
   render() {
     return (
-      <div className="App">
-		
-      </div>
+      <Query query={BOOKS_QUERY}>
+		{ ({loading, error, data}) => {
+			if(loading) return <p>Loading books</p>;
+			if(error) return <h4>Error loading books</h4>;
+			if(data) {
+				return data.books.map(book => <p key={book.id}>{book.title}, {book.price}</p>);
+			}
+		}}	
+	  </Query>
     );
   }
 }
+
+const BOOKS_QUERY = gql`{
+	books {
+		id, title, price
+	}
+}`;
 
 export default BookList;
