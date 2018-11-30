1f5bc400a5e1fc43620b66af0ac26ff2ea4e9afe
prabhu-durasoft <iamprabhu@yahoo.com>
2018-11-30 13:36:46 +0530

    Day03

--- Day03/day03_apollo_react_client/src/App.js
+++ Day03/day03_apollo_react_client/src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+		<h3>Add a new Book</h3>
 		<AddBook/>
+		<hr/>
 		<h1>List of Books</h1>
 		<hr/>
        	<BookList/>
